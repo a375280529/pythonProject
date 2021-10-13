5f7459aba9397a547ad15465a9d593216cb9b0c8
vzoom <375280529@qq.com>
2021-10-13 15:44:24 +0800

    信息

--- forpub/finalClass.py
+++ forpub/finalClass.py
@@ -7,11 +7,51 @@ import requests
 import json
 import os
 import configparser
+import sys
+
+def maopao(lists):
+    # 冒泡排序
+    count = len(lists)
+    for i in range(0, count):
+        for j in range(i + 1, count):
+            if lists[i] > lists[j]:
+                lists[i], lists[j] = lists[j], lists[i]
+    return lists
+
+
+def quzero(val):
+    try:
+        dex=val.index('.')
+        listv=val.split('.')
+        lista=[]
+        listb=[]
+        biaoshi=0
+        value=listv[0]
+        ot=""
+        for i in listv[1]:
+            lista.append(i)
+        for b in reversed(lista):
+            if biaoshi==0:
+                if b!="0":
+                    listb.append(b)
+                    biaoshi=1
+                else:
+                    pass
+            else:
+                listb.append(b)
+        for c in reversed(listb):
+            ot+=c
+        if ot=="":
+            pass
+        else:
+            value+="."+ot
+        return value
+    except:
+        return val
 
 #读取原始单元格格式的excel
 def readExcelRealGeShi(url):
-    xl = pd.ExcelFile(url)
-    df = xl.parse(0)
+    df = pd.read_excel(url, sheet_name=0, converters={'sw_jcxx_hy':str,'sw_jcxx_nsrztdm':str})
     list = []
     for i in range(len(df)):
         map = {}
@@ -24,14 +64,14 @@ def readExcelRealGeShi(url):
                 map[name] = int(df[name][i])
             elif isinstance(df[name][i], nm.floating):
                 if str(df[name][i])=="nan":
-                    map[name] = None
+                    map[name] = 0.0
                 else:
                     map[name] = float(df[name][i])
             else:
                 if str(df[name][i])=="nan":
-                    map[name] = None
+                    map[name] = ''
                 else:
-                    map[name] = df[name][i]
+                    map[name] = quzero(str(df[name][i]))
         list.append(map)
     return list
 
@@ -67,6 +107,7 @@ def readini(ini):
 
 # 获取配置文件相对路径
 def getIniPath():
-    path = os.path.dirname(os.path.abspath(__file__))
-    path = path.split(path.split('\\')[-1])[0] + "iniFile" + "\\"
+    exepath = os.path.realpath(sys.executable)
+    endpath = exepath.split(exepath.split("\\")[-1])[0]
+    path=os.path.join(os.path.abspath('.'), endpath + "iniFile\\")
     return path
